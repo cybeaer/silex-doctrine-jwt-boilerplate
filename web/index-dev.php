3f6c359432af557f354b67c1d730d02be98a20e2
Michael Attenberger <attenberger@gmail.com>
2018-03-18 16:57:05 +0100

    added orm service provider

--- web/index-dev.php
+++ web/index-dev.php
@@ -1,9 +1,48 @@
 <?php
 use Silex\Application;
+use Doctrine\Common\Annotations\AnnotationRegistry;
+use Silex\Provider\DoctrineServiceProvider;
+use Dflydev\Provider\DoctrineOrm\DoctrineOrmServiceProvider;
 
-require_once __DIR__ . './../vendor/autoload.php';
+$baseDir = __DIR__ . '/../';
+$loader = require $baseDir . '/vendor/autoload.php';
 
 $app = new Application();
+
+AnnotationRegistry::registerLoader([$loader, 'loadClass']);
+$app->register(
+    new DoctrineServiceProvider(),
+    [
+        'db.options' => [
+            'driver'        => 'pdo_mysql',
+            'host'          => 'localhost',
+            'dbname'        => 'sample',
+            'user'          => 'root',
+            'password'      => 'admin',
+            'charset'       => 'utf8',
+            'driverOptions' => [
+                1002 => 'SET NAMES utf8',
+            ],
+        ],
+    ]
+);
+
+$app->register(
+    new DoctrineOrmServiceProvider(),
+    [
+        'orm.proxies_dir'             => $baseDir . 'src/Entity/Proxy',
+        'orm.auto_generate_proxies'   => $app['debug'],
+        'orm.em.options'              => [
+            'mappings' => [
+                [
+                    'type'                         => 'annotation',
+                    'namespace'                    => 'mgcom\\Entity\\',
+                    'path'                         => $baseDir. 'src/Entity',
+                    'use_simple_annotation_reader' => false,
+                ],
+            ],
+    ]
+]);
 
 Symfony\Component\Debug\Debug::enable();
 $app['debug'] = true;

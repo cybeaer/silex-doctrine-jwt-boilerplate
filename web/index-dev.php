0b00e2d39a9db0db809791084a69a045d1692943
Michael Attenberger <attenberger@gmail.com>
2018-03-18 17:06:56 +0100

    added check for only local dev calls

--- web/index-dev.php
+++ web/index-dev.php
@@ -4,6 +4,13 @@ use Doctrine\Common\Annotations\AnnotationRegistry;
 use Silex\Provider\DoctrineServiceProvider;
 use Dflydev\Provider\DoctrineOrm\DoctrineOrmServiceProvider;
 
+if (isset($_SERVER['HTTP_CLIENT_IP']) || isset($_SERVER['HTTP_X_FORWARDED_FOR']) ||
+    !in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1')))
+{
+    header('HTTP/1.0 403 Forbidden');
+    exit('403 Forbidden.');
+}
+
 $baseDir = __DIR__ . '/../';
 $loader = require $baseDir . '/vendor/autoload.php';
 
